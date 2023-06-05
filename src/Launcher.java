027618320ea4531699bf5e184411628217d553ae
Santiago Rios Valero <riossanval@gmail.com>
2023-06-04 19:52:17 -0500

    initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,17 @@
+
+
+import presentation.Model;
+
+public class Launcher {
+
+    private Model miApp;
+
+    public Launcher() {
+        miApp = new Model();
+        miApp.init();
+    }
+    
+    public static void main(String[] args) {      
+        new Launcher();
+    }
+}
