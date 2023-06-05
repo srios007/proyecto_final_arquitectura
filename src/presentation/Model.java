6d722ca24675e4499ecba3ac16e0f1c12ef70d14
Santiago Rios Valero <riossanval@gmail.com>
2023-06-04 21:32:20 -0500

    [ADD] base panel

--- src/presentation/Model.java
+++ src/presentation/Model.java
@@ -1,39 +1,72 @@
 package presentation;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
-public class Model {
+public class Model extends JFrame {
 
-    private JFrame principalWindow;
-    private boolean runApplication;
+    public Model() {
+        // Configuración básica del JFrame
+        setTitle("Proyecto final arquitectura");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1300, 800));
 
-    public Model() {}
+        // Crear paneles para los módulos
+        JPanel inputPanel = new JPanel();
+        JPanel buttonsPanel = new JPanel();
+        JPanel tablePanel = new JPanel();
+        JPanel controlWordsAndClock = new JPanel();
+        JPanel textArea = new JPanel();
+        JPanel clockOutput = new JPanel();
+        JPanel controlWords = new JPanel();
 
-    public void init() {
-        getPrincipalWindow().pack();
-        getPrincipalWindow().setVisible(true);
-    }
+        // Establecer el tamaño de los paneles
+        inputPanel.setPreferredSize(new Dimension(400, 400));
+        buttonsPanel.setPreferredSize(new Dimension(200, 400));
+        tablePanel.setPreferredSize(new Dimension(600, 400));
+        controlWordsAndClock.setPreferredSize(new Dimension(600, 310));
+        textArea.setPreferredSize(new Dimension(600, 310));
+        clockOutput.setPreferredSize(new Dimension(600, 150));
+        controlWords.setPreferredSize(new Dimension(600, 150));
 
-    // genera la ventana principal, que contendrá a los diferentes paneles que 
-    // mostrarán el resultado de la ejecución del simulador
-    public JFrame getPrincipalWindow() {
-        if (principalWindow == null) {
-            principalWindow = new JFrame();
-            principalWindow.setTitle("Proyect final arquitectura");
-            principalWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            principalWindow.setPreferredSize(new Dimension(1200, 768));
-            principalWindow.setResizable(true);
-        }
-        return principalWindow;
-    }
+        // Crear un borde personalizado
+        javax.swing.border.Border borde = BorderFactory.createLineBorder(Color.RED, 2);
 
-    public boolean isApplicationRunning() {
-        return runApplication;
-    }
+        // Establecer el borde de los paneles
+        inputPanel.setBorder(borde);
+        buttonsPanel.setBorder(borde);
+        tablePanel.setBorder(borde);
+        textArea.setBorder(borde);
+        clockOutput.setBorder(borde);
+        controlWords.setBorder(borde);
 
-    public void setIsRunningApplication(boolean auto) {
-        this.runApplication = auto;
+        // Agregar componentes a los paneles
+//        inputPanel.add(new JButton("Botón 1"));
+//        buttonsPanel.add(new JLabel("Etiqueta 1"));
+        // Agregar los paneles al JFrame
+        getContentPane().add(inputPanel);
+        getContentPane().add(buttonsPanel);
+        getContentPane().add(tablePanel);
+        getContentPane().add(controlWordsAndClock);
+        getContentPane().add(textArea);
+        controlWordsAndClock.add(clockOutput);
+        controlWordsAndClock.add(controlWords);
+
+        // Establecer un administrador de diseño (layout) para los paneles
+        getContentPane().setLayout(new FlowLayout());
+
+        // Centrar el JFrame en la pantalla
+        setLocationRelativeTo(null);
+        pack();
+
+        // Mostrar el JFrame
+        setVisible(true);
     }
 
 }
